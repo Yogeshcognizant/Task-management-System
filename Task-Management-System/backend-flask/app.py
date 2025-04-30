40faead245a38a23188eae05b35433f972c692da
Yogeshcognizant <127288675+Yogeshcognizant@users.noreply.github.com>
2025-04-30 10:28:04 +0530

    Update app.py

--- Task-Management-System/backend-flask/app.py
+++ Task-Management-System/backend-flask/app.py
@@ -1,22 +1,24 @@
 from flask import Flask
-from tasks.email_remainder import send_remainder
+
 from apscheduler.schedulers.background import BackgroundScheduler
+from tasks.email_remainder import send_remainder
+import atexit
 
 app = Flask(__name__)
 
-def schedule_task():
-    send_remainder()
-
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=send_remainder, trigger="interval", hours=1)
+scheduler.add_job(func=send_remainder, trigger="interval", hours=5)
 scheduler.start()
 
+def shutdown_scheduler():
+    if scheduler.running:
+        scheduler.shutdown()
+
+atexit.register(shutdown_scheduler)
 @app.route('/')
-def home():
+def index():
     return "Flask server running. Sending emails every 1 hour!"
 
 if __name__ == "__main__":
-    try:
-        app.run(port=5000)
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
+    app.run(debug=True, port=5001)
+    
