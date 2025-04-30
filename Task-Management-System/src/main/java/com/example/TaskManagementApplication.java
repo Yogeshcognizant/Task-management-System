6a7e3f25edb5fc1e45769128d712df5dc755bd27
Yogeshcognizant <127288675+Yogeshcognizant@users.noreply.github.com>
2025-04-30 10:04:22 +0530

    Update TaskManagementApplication.java

--- Task-Management-System/src/main/java/com/example/TaskManagementApplication.java
+++ Task-Management-System/src/main/java/com/example/TaskManagementApplication.java
@@ -1,16 +1,23 @@
 package com.example.taskmanagement;
-
+import com.example.taskmanagement.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+public class TaskManagementApplication implements CommandLineRunner{
+   @Autowired
+  private TaskService taskService;
+  public static void main(String[] args){
+  SpringApplication.run(TaskManagementApplication.class,args);
+  }
 
-public class TaskManagementApplication{
-
-public static void main(String[] args){
-
-SpringApplication.run(TaskManagementApplication.class,args);
-}
+  @Override
+  public void run(String... args) throws Exception{
+    System.out.println("Starting Flask scheduler...");
+    taskService.startScheduler();
+    System.out.println("schedule started successfully.");
+  }
 
 }
