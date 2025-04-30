94ff22df5603719c9708cae45c25caaf282b6589
Yogeshcognizant <127288675+Yogeshcognizant@users.noreply.github.com>
2025-04-30 10:06:57 +0530

    Update TaskRepository.java

--- Task-Management-System/taskmanagement/repository/TaskRepository.java
+++ Task-Management-System/taskmanagement/repository/TaskRepository.java
@@ -4,5 +4,7 @@ import com.example.taskmanagement.model.Task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
 public interface TaskRepository extends JpaRepository<Task,Long>{
 }
